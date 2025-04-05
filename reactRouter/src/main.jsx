5ac88580cfff464f537be06d3b59c5fb56836a1d
rayogoda <ray.ogoda@evolany.com>
2025-04-05 09:04:09 +0900

    Add initial stuff

---
+++ reactRouter/src/main.jsx
@@ -0,0 +1,29 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Profile from "./Profile";
+import Popeye from "./Popeye";
+import Spinach from "./Spinach";
+
+const router = createBrowserRouter([
+      {
+              path: "/",
+              element: <App />,
+            },
+      {
+              path: "profile",
+              element: <Profile />,
+              children: [
+                  {path: "spinach", element: <Spinach />},
+                  {path: "popeye", element: <Popeye />},
+              ],
+            },
+]);
+
+createRoot(document.getElementById("root")).render(
+      <StrictMode>
+        <RouterProvider router={router} />
+      </StrictMode>
+);
+
